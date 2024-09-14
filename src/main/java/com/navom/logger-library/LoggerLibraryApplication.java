35268dfd37d5b5358a1a2a7b1f8a54a2d2a4887b
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-09-14 22:07:43 +0530

    .

---
+++ src/main/java/com/navom/logger-library/LoggerLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.navom.boilerplate_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navom/boilerplate_java/BoilerplateJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navom.boilerplate_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
