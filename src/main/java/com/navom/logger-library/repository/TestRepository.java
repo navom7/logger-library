6519cb3476548c41a1783568113f42f3600a3219
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-09-14 22:09:41 +0530

    .

--- src/main/java/com/navom/logger-library/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.navom.boilerplate_java.repository;
-
-import com.navom.boilerplate_java.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<TestEntity, Integer> {
-}
