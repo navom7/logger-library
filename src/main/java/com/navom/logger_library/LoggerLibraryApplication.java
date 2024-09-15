bc8d5803adc5dfc63e9247ed6878513dadbd0c7d
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-09-15 07:43:26 +0530

    initial Setup

---
+++ src/main/java/com/navom/logger_library/LoggerLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.navom.logger_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navom/logger_library/LoggerLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navom.logger_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
